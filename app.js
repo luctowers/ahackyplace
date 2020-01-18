20164aabf0a98b10a8e7193b72ec9bb22e00b79c
Lucas Towers <luctowers@gmail.com>
2020-01-18 00:13:26 -0800

    Installed Redis client and added connection code

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
 const express = require("express");
+const redis = require("redis");
+const config = require("./config");
+
+// REDIS
+
+const redisClient = redis.createClient({
+  host: config.redishost,
+  port: config.redisPort
+});
+
+redisClient.on("connect", () => {
+  console.log("Connection established to Redis server!");
+});
+
+redisClient.on("error", (error) => {
+  console.log("Redis error: " + error);
+});
+
+// EXPRESS
+
 const app = express();
 
 app.use(express.static("static"));
--- config.js
+++ config.js
@@ -2,7 +2,9 @@ const isProduction = process.env.NODE_ENV == "production";
 
 var config = {
   isProduction: isProduction,
-  port: isProduction ? process.env.PORT : 8080
+  port: isProduction ? process.env.PORT : 8080,
+  redisHost: isProduction ? process.env.REDIS_HOST : "localhost",
+  redisPort: isProduction ? process.env.REDIS_PORT : 6379
 };
 
 module.exports = config;
