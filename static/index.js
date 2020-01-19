06c60cd5744c29927d7d0bdbfd8629e013446a46
Lucas Towers <luctowers@gmail.com>
2020-01-19 13:57:04 -0800

    Cleaned up and overhauled client side JS

--- static/index.js
+++ static/index.js
@@ -1,78 +1,122 @@
+// CONSTANTS (can't const because of browser compatibility)
+
 var colors = ["#ffffff", "#e4e4e4", "#888888", "#222222", "#ffa7d1", "#e50000", "#e59500", "#a06a42", "#e5d900", "#94e044", "#02be01", "#00d3dd", "#0083c7", "#0000ea", "#cf6ee4", "#820080"];
 var canvasMargin = 64;
+var reconnectDelay = 1000;
 
-var canvas = document.getElementById("canvas");
-var container = document.getElementById("container");
+// DOM ELEMENTS
 
-var canvasData = null;
+var canvasElement = document.getElementById("canvas");
+var containerElement = document.getElementById("container");
 
-var websocket = new WebSocket("ws://" + window.location.host + "/");
-websocket.binaryType = "arraybuffer";
-websocket.onmessage = function(message) {
-  var bufferView32 = new Uint32Array(message.data);
-  var x = bufferView32[0];
-  var y = bufferView32[1];
-  var value = bufferView32[2];
-  if (canvasData) {
-    canvasData.pixels[x+y*canvasData.width] = value;
-    redrawPixel(x, y);
-  }
-};
+// VARIABLES
+
+var websocket;
+var pendingPixels = [];
+var canvasPending = true;
+var canvasPixels = [];
+var canvasWidth = 0;
+var canvasHeight = 0;
+
+// HANDLE RESIZES BY RESIZING AND REDRAWING CANVAS
 
 function onResize() {
-  if (!canvasData)
+  if (!canvasWidth || !canvasHeight)
     return;
-  var containerAspectRatio = container.offsetWidth / container.offsetHeight;
-  var canvasAspectRatio = canvasData.width/canvasData.height;
+  var containerAspectRatio = containerElement.offsetWidth / containerElement.offsetHeight;
+  var canvasAspectRatio = canvasWidth / canvasHeight;
 
   if (containerAspectRatio < canvasAspectRatio) {
-    canvas.width = container.offsetWidth - canvasMargin;
-    canvas.height = canvas.width/canvasAspectRatio;
+    canvasElement.width = containerElement.offsetWidth - canvasMargin;
+    canvasElement.height = canvasElement.width/canvasAspectRatio;
   }
   else {
-    canvas.height = container.offsetHeight - canvasMargin;
-    canvas.width = canvas.height*canvasAspectRatio;
+    canvasElement.height = containerElement.offsetHeight - canvasMargin;
+    canvasElement.width = canvasElement.height*canvasAspectRatio;
   }
 
   redrawFullCanvas();
 }
 
-function onLoad() {
+// WEBSOCKET CODE (listens for newly placed pixels)
 
-  fullCanvas(function(error, response) {
+function openNewWebsocket() {
+  // create the socket
+  websocket = new WebSocket("ws://" + window.location.host + "/");
+  websocket.binaryType = "arraybuffer";
 
-    if (error) {
-      alert(error);
-      return;
-    }
+  // on open we will automattically be listening for canvas updates
+  // after that get a copy of the full canvas and we will add the new pixels
+  // once it arrives
+  websocket.onopen = function() {
+    apiFullCanvas(function(error, canvasData) {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      canvasPending = false;
+      canvasWidth = canvasData.width;
+      canvasHeight = canvasData.height;
+      canvasPixels = canvasData.pixels;
+      flushPendingPixels();
 
-    canvasData = response;
-    onResize();
-    canvas.style.visibility = "visible";
+      onResize();
+      canvas.style.visibility = "visible";
+    })
+  };
 
-  });
+  // attempt to re-establish the connection if it drops
+  function attemptRecconect() {
+    pendingPixels = [];
+    canvasPending = true;
+    setTimeout(openNewWebsocket, reconnectDelay);
+  }
+  websocket.onclose = attemptRecconect;
+  websocket.onerror = attemptRecconect;
+
+  // handle new data from the server
+  websocket.onmessage = function(message) {
+    var bufferView32 = new Uint32Array(message.data);
+    pendingPixels.push(bufferView32);
+    if (!canvasPending)
+      flushPendingPixels();
+  };
+}
+
+// CANVAS DRAWING FUNCTIONS
+
+function flushPendingPixels() {canvas
+
+  for (var p = 0; p < pendingPixels.length; p++) {
+    var pixelX = pendingPixels[p][0];
+    var pixelY = pendingPixels[p][1];
+    var pixelValue = pendingPixels[p][2];
+    canvasPixels[pixelX+pixelY*canvasWidth] = pixelValue;
+    redrawPixel(pixelX, pixelY);
+  }
+  pendingPixels = [];
 
 }
 
 function redrawFullCanvas() {
 
-  for (var x = 0; x < canvasData.width; x++)
-    for (var y = 0; y < canvasData.height; y++)
+  for (var x = 0; x < canvasWidth; x++)
+    for (var y = 0; y < canvasHeight; y++)
       redrawPixel(x, y);
 
 }
 
 function redrawPixel(x, y) {
 
-  if (typeof canvas.getContext == "function") {
+  if (typeof canvasElement.getContext == "function") {
     var ctx = canvas.getContext("2d");
 
-    var rectX = Math.floor(x * canvas.width / canvasData.width);
-    var rectY = Math.floor(y * canvas.height / canvasData.height);
-    var rectWidth = Math.ceil(canvas.width / canvasData.width);
-    var rectHeight = Math.ceil(canvas.height / canvasData.height);
+    var rectX = Math.floor(x * canvasElement.width / canvasWidth);
+    var rectY = Math.floor(y * canvasElement.height / canvasHeight);
+    var rectWidth = Math.ceil(canvasElement.width / canvasWidth);
+    var rectHeight = Math.ceil(canvasElement.height / canvasHeight);
 
-    var value = canvasData.pixels[x+y*canvasData.width];
+    var value = canvasPixels[x+y*canvasWidth];
     ctx.fillStyle = colors[value];
 
     ctx.fillRect(rectX, rectY, rectWidth, rectHeight);
@@ -80,10 +124,9 @@ function redrawPixel(x, y) {
 
 }
 
-window.onload = onLoad;
-window.onresize = onResize;
+// API FUNCTIONS
 
-function setPixel(x, y, value, callback) {
+function apiSetPixel(x, y, value, callback) {
   var xhr = new XMLHttpRequest();
   xhr.open("POST", "/api/setpixel", true);
   xhr.setRequestHeader("Content-Type", "application/json");
@@ -99,30 +142,30 @@ function setPixel(x, y, value, callback) {
   xhr.send(JSON.stringify({x: x, y: y, value: value}));
 }
 
-function fullCanvas(callback) {
+function apiFullCanvas(callback) {
 
   function processBinaryCanvas(buffer) {
     try {
       var bufferView8 = new Uint8Array(buffer);
       var bufferView32 = new Uint32Array(buffer);
-      var canvas = {};
-      canvas.width = bufferView32[0];
-      canvas.height = bufferView32[1];
-      canvas.bitdepth = bufferView32[2];
-      canvas.pixels = [];
+      var canvasData = {};
+      canvasData.width = bufferView32[0];
+      canvasData.height = bufferView32[1];
+      canvasData.bitdepth = bufferView32[2];
+      canvasData.pixels = [];
 
-      if (canvas.bitdepth != 4)
+      if (canvasData.bitdepth != 4)
         throw new Error("Unsupported canvas bitdepth");
 
-      for (var p = 0; p < canvas.width*canvas.height; p++) {
+      for (var p = 0; p < canvasData.width*canvasData.height; p++) {
         var byteIndex = 12 + (p >> 1);
         if (p & 1)
-          canvas.pixels.push(bufferView8[byteIndex] & 0xF);
+          canvasData.pixels.push(bufferView8[byteIndex] & 0xF);
         else
-          canvas.pixels.push(bufferView8[byteIndex] >> 4);
+          canvasData.pixels.push(bufferView8[byteIndex] >> 4);
       }
 
-      callback(null, canvas);
+      callback(null, canvasData);
     }
     catch (error) {
       callback(error, null);
@@ -144,3 +187,8 @@ function fullCanvas(callback) {
   xhr.send();
 
 }
+
+// SET HANDLERS
+
+window.onload = openNewWebsocket;
+window.onresize = onResize;
