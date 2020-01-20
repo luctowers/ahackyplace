9e6a680ed368f13f42e11fa49768c5ed3b6bfcfe
Lucas Towers <luctowers@gmail.com>
2020-01-19 18:35:19 -0800

    Fixed typo

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const WindowBatch = require("./lib/window-batch");
 const server = http.createServer(app);
 const wss = new WebSocket.Server({server});
 
-// send new pixels to client in batches, one btach repressents all new pixels in a 100ms timeframe
+// send new pixels to client in batches, one batch represents all new pixels in a 100ms timeframe
 var newPixelBatch = new WindowBatch(100, (batch) => {
   wss.clients.forEach((client) => {
     if (client.readyState === WebSocket.OPEN)
